87d97eabf156014e2e5f3f5e558e085917081f49
0xpablo <pablos93@gmail.com>
2025-12-18 19:53:44 +0100

    libfsapfs: update to fork sealed+zbitmap

--- Formula/libfsapfs.rb
+++ Formula/libfsapfs.rb
@@ -1,17 +1,16 @@
 class Libfsapfs < Formula
   desc "Library to access the Apple File System (APFS)"
-  homepage "https://github.com/libyal/libfsapfs"
-  url "https://github.com/libyal/libfsapfs/releases/download/20240429/libfsapfs-experimental-20240429.tar.gz"
-  sha256 "665f70705e69ce90c87e0f9c185d47002e242b79b0a37ae52751cdfa8363a538"
+  homepage "https://github.com/0xpablo/libfsapfs"
+  url "https://github.com/0xpablo/libfsapfs/archive/192ede6f30b2a413d01731a4bdab52d0f94ac9a5.tar.gz"
+  sha256 "05c4a9a24b9e99e088e112a9466bfa43b1fc0c1ee0d63d827900b6dc228a1aa6"
   license "LGPL-3.0-or-later"
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "gettext" => :build
+  depends_on "libtool" => :build
   depends_on "pkgconf" => :build
   depends_on "openssl@3"
-
-  # Fixes a build failure in the vendored libcaes when OpenSSL headers are
-  # available but EVP AES support is not detected, by ensuring the local AES
-  # implementation is compiled in that situation.
-  patch :DATA
 
   def install
     openssl = Formula["openssl@3"]
@@ -20,6 +19,7 @@ class Libfsapfs < Formula
     ENV.append "LDFLAGS", "-L#{openssl.opt_lib}"
     ENV.prepend_path "PKG_CONFIG_PATH", "#{openssl.opt_lib}/pkgconfig"
 
+    system "autoreconf", "-fi"
     system "./configure", *std_configure_args,
                           "--with-openssl=#{openssl.opt_prefix}",
                           "--enable-openssl-evp-cipher",
@@ -28,28 +28,6 @@ class Libfsapfs < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/fsapfsinfo -V")
+    assert_match "fsapfsinfo", shell_output("#{bin}/fsapfsinfo -V")
   end
 end
-
-__END__
-diff --git a/libcaes/libcaes_definitions.h b/libcaes/libcaes_definitions.h
-index 296b431..4fd3e9f 100644
---- a/libcaes/libcaes_definitions.h
-+++ b/libcaes/libcaes_definitions.h
-@@ -50,12 +50,14 @@ enum LIBCAES_CRYPT_MODES
- };
- 
- #endif /* !defined( HAVE_LOCAL_LIBCAES ) */
- 
--#if defined( HAVE_LIBCRYPTO ) && defined( HAVE_OPENSSL_AES_H )
-+#if defined( HAVE_LIBCRYPTO ) && defined( HAVE_OPENSSL_AES_H ) \
-+ && ( defined( HAVE_AES_CBC_ENCRYPT ) || defined( HAVE_AES_ECB_ENCRYPT ) )
- #define LIBCAES_HAVE_AES_SUPPORT
- 
--#elif defined( HAVE_LIBCRYPTO ) && defined( HAVE_OPENSSL_EVP_H )
-+#elif defined( HAVE_LIBCRYPTO ) && defined( HAVE_OPENSSL_EVP_H ) \
-+ && ( defined( HAVE_EVP_CRYPTO_AES_CBC ) || defined( HAVE_EVP_CRYPTO_AES_ECB ) )
- #define LIBCAES_HAVE_AES_SUPPORT
- 
- #endif
